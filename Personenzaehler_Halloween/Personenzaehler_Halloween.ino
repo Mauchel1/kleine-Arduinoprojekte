d6317e7939b8816d640f81e74958e7f8e978c8cb
Daniel <Daniel.Friedrich@haw-hamburg.de>
2015-05-12 18:50:28 +0200

    Programmbeschreibung hinzugefügt

--- Personenzaehler_Halloween/Personenzaehler_Halloween.ino
+++ Personenzaehler_Halloween/Personenzaehler_Halloween.ino
@@ -1,3 +1,9 @@
+/*
+Dieses Programm soll ähnlich einer Lichtschranke Personen zählen
+Auch soll die Anzahl von Knopfdrücken gezählt werden. 
+Die Anzeige erfolgt mit einer 3fachen 7 Segment Anzeige
+*/
+
 void setup() {
   // put your setup code here, to run once:
 
