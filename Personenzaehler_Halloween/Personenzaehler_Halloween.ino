c3410012765eb6deb5bfe6b49cc09283af547305
Daniel <Daniel.Friedrich@haw-hamburg.de>
2015-05-13 01:14:56 +0200

    Neue Funktionen hinzugefügt
    Der Lichtsensor wurde ins System integriert

--- Personenzaehler_Halloween/Personenzaehler_Halloween.ino
+++ Personenzaehler_Halloween/Personenzaehler_Halloween.ino
@@ -4,13 +4,13 @@ Auch soll die Anzahl von Knopfdrücken gezählt werden.
 Die Anzeige erfolgt mit einer 3fachen 7 Segment Anzeige
 */
 
-//7-Segment-LED
-    #define SEGMENT1_PIN 47 // Ground von Segment 1
-    #define SEGMENT2_PIN 48 // Ground von Segment 2
-    #define SEGMENT3_PIN 49 // Ground von Segment 3
-    #define CLOCK_PIN 40 // toggeln während der Datenübertragung
-    #define LATCH_PIN 41 // vor dem Senden der Daten an den IC auf LOW ziehen, danach wieder HIGH
-    #define DATA_PIN 44  // an den seriellen Eingang des IC; jeweils 1 Byte übertragen
+    #define SEGMENT1_PIN 8 // Ground von Segment 1
+    #define SEGMENT2_PIN 9 // Ground von Segment 2
+    #define SEGMENT3_PIN 10 // Ground von Segment 3
+    #define CLOCK_PIN 7 // toggeln während der Datenübertragung
+    #define LATCH_PIN 6 // vor dem Senden der Daten an den IC auf LOW ziehen, danach wieder HIGH
+    #define DATA_PIN 5  // an den seriellen Eingang des IC; jeweils 1 Byte übertragen
+    #define LIGHT_PIN 4
    
     uint8_t zahlen[10] = {0x3f,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F};
 
@@ -18,6 +18,11 @@ Die Anzeige erfolgt mit einer 3fachen 7 Segment Anzeige
     int einer = 0;
     int zehner = 0;
     int hunderter = 0;
+    unsigned long time = 0;
+    int licht = 0;
+    int lichtalt = 0;
+    int flanke = 0;
+    int wartezeit = 1200; // blindzeit des Lichtsensors in Millisekunden
 
 void setup() {
     pinMode(CLOCK_PIN, OUTPUT);
@@ -26,6 +31,8 @@ void setup() {
     pinMode(SEGMENT1_PIN, OUTPUT);
     pinMode(SEGMENT2_PIN, OUTPUT);
     pinMode(SEGMENT3_PIN, OUTPUT);
+    pinMode(LIGHT_PIN, INPUT);  
+
 }
 
 
@@ -57,8 +64,18 @@ void write_to_segments(){
         shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST,zahlen[einer]);
 }
 
-void loop() {
-        
-    write_to_segments();
+void light_status(){
+    licht = digitalRead(LIGHT_PIN);
+    if (licht != lichtalt && licht) {
+        if (millis() - time > wartezeit){
+            zahl++;
+            time = millis();
+        }
+    }
+    lichtalt = licht;
+}
 
+void loop() {
+    light_status();
+    write_to_segments();
 }
